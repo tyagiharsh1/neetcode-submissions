739d3c9e4bbf5fee730a48121e3e5c70d27932a0
Harsh Tyagi <141999553+tyagiharsh1@users.noreply.github.com>
2026-07-20 17:03:17 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+
+        int count[26]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++ )
+        {
+            if(count[t[i]-'a']==0)
+            {
+                return false;
+            }
+            count[t[i]-'a']--;
+
+        }
+        return true;
+    }
+};
