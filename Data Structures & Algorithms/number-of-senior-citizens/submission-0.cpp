3575c067297c195e02f7307fa9d3d9e750ccf6c4
Harsh Tyagi <141999553+tyagiharsh1@users.noreply.github.com>
2026-07-22 23:04:35 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int res = 0;
+        for (const string& d : details) {
+            if (stoi(d.substr(11, 2)) > 60) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
